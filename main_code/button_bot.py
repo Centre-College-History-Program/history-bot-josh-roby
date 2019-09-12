9cd94bfb57968dae4d59fdf445a736a8f4f90c19
John Harney <john.harney@centre.edu>
2019-09-12 09:10:21 -0400

    Working version

--- main_code/button_bot.py
+++ main_code/button_bot.py
@@ -12,29 +12,28 @@ import time
 import os
 import random
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM) # use gpio numbering
+GPIO.setmode(GPIO.BCM) #use the GPIO numbering
+GPIO.setwarnings(False) # Avoids warning channel is already in use
 
 led = 21 # GPIO pin 21
+button = 18 # GPIO pin 18
+
+GPIO.setup(led,GPIO.OUT) # sets up pin 21 to output
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets up pin 18 as a button
+
 myCmd1 = 'omxplayer -o alsa kennedy1.mp3' # These examples are from the "Kennedy bot"
 myCmd2 = 'omxplayer -o alsa kennedy2.mp3' # Change 
 myCmd3 = 'omxplayer -o alsa kennedy3.mp3' # as
 myCmd4 = 'omxplayer -o alsa kennedy4.mp3' # needed
 myList = [myCmd1, myCmd2, myCmd3, myCmd4] 
 
-# set pin 20 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(20, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) 
-
-
-current_state = 0
-GPIO.setup(led,GPIO.OUT)
-
 while True:
-    try:
-        if GPIO.input(20) == GPIO.HIGH:
-          GPIO.output(led,True) #Turn on LED
-          os.system(random.choice(myList)) # plays one of the mp3 files
-          print("History Bot Successfully Activated!")
-          GPIO.output(led,False) #turn off LED
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+        input_state = GPIO.input(button) # primes the button!
+        if input_state == False:
+            print("History Bot Activated!")
+            GPIO.output(led,True) #Turn on LED
+            os.system(random.choice(myList)) # play sound file
+            GPIO.output(led,False) #turn off LED
+            time.sleep(0.2)
+
+
