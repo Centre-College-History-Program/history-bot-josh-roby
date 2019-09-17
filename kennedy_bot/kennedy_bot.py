92757d469db3e0426efd3a3469c0f29ce4a6857d
John Harney <john.harney@centre.edu>
2019-09-17 14:21:13 -0400

    Did some more housekeeping, renamed some files.

--- kennedy_bot/kennedy_bot.py
+++ kennedy_bot/kennedy_bot.py
@@ -1,37 +1,38 @@
+# John Harney, Centre College, 09.10.19
+
 # this code exists thanks to Texas-Mark on the official RPi forums, 
-# he shared the code the original iteration of this was (heavily) based on at 
+# he shared the code the original (PIR sensor) iteration of this was based on at 
 # https://www.raspberrypi.org/forums/viewtopic.php?t=176241
+
+# button code version relied heavily on Soren at 
+# https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
 
 import RPi.GPIO as GPIO
 import time
 import os
 import random
 
+GPIO.setmode(GPIO.BCM) #use the GPIO numbering
+GPIO.setwarnings(False) # Avoids warning channel is already in use
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+led = 21 # GPIO pin 21
+button = 18 # GPIO pin 18
 
-pir_sensor = 4 #GPIO pin 4
-led = 21 #GPIO pin 21
-myCmd1 = 'omxplayer -o alsa kennedy1.mp3'
-myCmd2 = 'omxplayer -o alsa kennedy2.mp3'
-myCmd3 = 'omxplayer -o alsa kennedy3.mp3'
-myCmd4 = 'omxplayer -o alsa kennedy4.mp3'
-myList = [myCmd1, myCmd2, myCmd3, myCmd4]
+GPIO.setup(led,GPIO.OUT) # sets up pin 21 to output
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets up pin 18 as a button
 
-GPIO.setup(pir_sensor, GPIO.IN, GPIO.PUD_DOWN)
-
-current_state = 0
-GPIO.setup(led,GPIO.OUT)
+myCmd1 = 'omxplayer -o alsa /home/pi/historybots/kennedy_bot/kennedy1.mp3' # These examples are from the "Kennedy bot"
+myCmd2 = 'omxplayer -o alsa /home/pi/historybots/kennedy_bot/kennedy2.mp3' # Change 
+myCmd3 = 'omxplayer -o alsa /home/pi/historybots/kennedy_bot/kennedy3.mp3' # as
+myCmd4 = 'omxplayer -o alsa /home/pi/historybots/kennedy_bot/kennedy4.mp3' # needed
+myList = [myCmd1, myCmd2, myCmd3, myCmd4] 
 
 while True:
-    try:
-        time.sleep(0.1)
-        current_state = GPIO.input(pir_sensor)
-        if current_state == 1:
-          GPIO.output(led,True) #Turn on LED
-          os.system(random.choice(myList)) # plays one of the mp3 files
-          GPIO.output(led,False) #turn off LED
-          time.sleep(4) # wait 4 seconds for PIR to reset.
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+        input_state = GPIO.input(button) # primes the button!
+        if input_state == False:
+            print("History Bot Activated!")
+            GPIO.output(led,True) #Turn on LED
+            os.system(random.choice(myList)) # play sound file
+            GPIO.output(led,False) #turn off LED
+            time.sleep(0.2)
+
