4272357afcbf0c3c2ade886e89e513022aca28dc
John Harney <john.harney@centre.edu>
2019-10-11 20:49:11 -0400

    first attempt button led pulse
    Not sure if this will work.... while loop is set to run forever, so may not get to the button press code.

--- main_code/led_button_historybot_code.py
+++ main_code/led_button_historybot_code.py
@@ -7,6 +7,9 @@
 # button code version relied heavily on Soren at
 # https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
 
+# button led pulse code comes from Muhammad Aqib at
+# https://electronicshobbyists.com/raspberry-pi-pwm-tutorial-control-brightness-of-led-and-servo-motor/
+
 import RPi.GPIO as GPIO
 import time
 import os
@@ -17,9 +20,11 @@ GPIO.setwarnings(False) # Avoids warning channel is already in use
 
 led = 21 # GPIO pin 21
 button = 18 # GPIO pin 18
+button_led = 26 # GPIO pin
 
 GPIO.setup(led,GPIO.OUT) # sets up pin 21 to output
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets up pin 18 as a button
+GPIO.setup(button_led, GPIO.OUT) # sets up pin 26 to output
 
 myCmd1 = 'omxplayer --volume 602 -o alsa /home/pi/historybots/ali_bot/ali1.mp3' # this clip was too low, changed volume
 myCmd2 = 'omxplayer -o alsa /home/pi/historybots/ali_bot/ali2.mp3' # These examples are from the "Ali bot"
@@ -29,6 +34,21 @@ myCmd5 = 'omxplayer -o alsa /home/pi/historybots/ali_bot/ali5.mp3' # needed
 myCmd6 = 'omxplayer -o alsa /home/pi/historybots/ali_bot/ali6.mp3'
 myList = [myCmd1, myCmd2, myCmd3, myCmd4, myCmd5, myCmd6]
 i_count = 0
+
+# all code below related to the pwm - including comments! - credit to Muhammad Aqib; see link above
+
+pwm = GPIO.PWM(button_led, 100)    # Created a PWM object
+pwm.start(0)                    # Started PWM at 0% duty cycle
+
+try:
+    while 1:                    # Loop will run forever
+        for x in range(100):    # This Loop will run 100 times
+            pwm.ChangeDutyCycle(x) # Change duty cycle
+            sleep(0.01)         # Delay of 10mS
+            
+        for x in range(100,0,-1): # Loop will run 100 times; 100 to 0
+            pwm.ChangeDutyCycle(x)
+            sleep(0.01)
 
 while True:
         input_state = GPIO.input(button) # primes the button!
