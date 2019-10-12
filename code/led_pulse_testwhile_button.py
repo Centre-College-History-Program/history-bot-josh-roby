44387fa408ed21ba928c1bdc5080eade16be0f22
John Harney <john.harney@centre.edu>
2019-10-12 13:06:08 -0400

    Fixed syntax error re: sleep function

--- code/led_pulse_testwhile_button.py
+++ code/led_pulse_testwhile_button.py
@@ -41,11 +41,11 @@ pwm.start(0)                    # Started PWM at 0% duty cycle
 while True:
         for x in range(100):    # This Loop will run 100 times
             pwm.ChangeDutyCycle(x) # Change duty cycle
-            sleep(0.01)         # Delay of 10mS
+            time.sleep(0.01)         # Delay of 10mS
 
         for x in range(100,0,-1): # Loop will run 100 times; 100 to 0
             pwm.ChangeDutyCycle(x)
-            sleep(0.01)
+            time.sleep(0.01)
 
         input_state = GPIO.input(button) # primes the button!
         if input_state == False:
