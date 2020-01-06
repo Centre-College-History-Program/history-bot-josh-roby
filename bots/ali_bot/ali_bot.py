ae2dbb635c5ca73da1b5731d992d368e46b3900b
John Harney <john.harney@centre.edu>
2020-01-06 14:19:52 -0500

    Updating links

--- bots/ali_bot/ali_bot.py
+++ bots/ali_bot/ali_bot.py
@@ -1,4 +1,4 @@
-# John Harney, Centre College, 09.27.19
+# John Harney, Centre College, updated 10.21.19
 
 # this code exists thanks to Texas-Mark on the official RPi forums,
 # he shared the code the original (PIR sensor) iteration of this was based on at
@@ -6,6 +6,8 @@
 
 # button code version relied heavily on Soren at
 # https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
+
+# this is the base code for all history bots as of 10.21.19
 
 import RPi.GPIO as GPIO
 import time
@@ -17,25 +19,34 @@ GPIO.setwarnings(False) # Avoids warning channel is already in use
 
 led = 21 # GPIO pin 21
 button = 18 # GPIO pin 18
+button_led = 26 # GPIO pin 26
 
 GPIO.setup(led,GPIO.OUT) # sets up pin 21 to output
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets up pin 18 as a button
+GPIO.setup(button_led,GPIO.OUT) #sets up pin 26 to output
 
-myCmd1 = 'omxplayer --vol 602 -o alsa /home/pi/historybots/ali_bot/ali1.mp3' # this clip was too low, changed volume
-myCmd2 = 'omxplayer -o alsa /home/pi/historybots/ali_bot/ali2.mp3' # These examples are from the "Ali bot"
-myCmd3 = 'omxplayer -o alsa /home/pi/historybots/ali_bot/ali3.mp3' # change
-myCmd4 = 'omxplayer -o alsa /home/pi/historybots/ali_bot/ali4.mp3' # as
-myCmd5 = 'omxplayer -o alsa /home/pi/historybots/ali_bot/ali5.mp3' # needed
-myCmd6 = 'omxplayer -o alsa /home/pi/historybots/ali_bot/ali6.mp3'
+myCmd1 = 'omxplayer --vol 602 -o alsa ali1.mp3' # this clip was too low, changed volume
+myCmd2 = 'omxplayer -o alsa ali2.mp3' # These examples are from the "Ali bot"
+myCmd3 = 'omxplayer -o alsa ali3.mp3' # change
+myCmd4 = 'omxplayer -o alsa ali4.mp3' # as
+myCmd5 = 'omxplayer -o alsa ali5.mp3' # needed
+myCmd6 = 'omxplayer -o alsa ali6.mp3'
 myList = [myCmd1, myCmd2, myCmd3, myCmd4, myCmd5, myCmd6]
-i_count = 0
+
+i_count = 0 # set up for correct grammar in notification below
 
 while True:
+        GPIO.output(button_led, True) # turn on button led
         input_state = GPIO.input(button) # primes the button!
         if input_state == False:
             i_count = i_count + 1
+            GPIO.output(button_led, False) # turns off button led
             GPIO.output(led,True) #Turn on LED
             os.system(random.choice(myList)) # play sound file
             GPIO.output(led,False) #turn off LED
-            print("History Bot has been activated " + str(i_count) + " times!")
+            if i_count == 1:
+                print("History Bot has been activated " + str(i_count) + " time!")
+            else:
+                print("History Bot has been activated " + str(i_count) + " times!")
+            GPIO.output(button_led, True) # turns button led back on
             time.sleep(0.2)
